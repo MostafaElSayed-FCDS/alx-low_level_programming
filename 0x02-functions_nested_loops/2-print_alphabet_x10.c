9c4ce857c670e2231246ab8bcc503f8915b8b7fc
Mostafa Elsayed <mostafaelsayed2172000@gmail.com>
2023-03-21 15:09:04 -0700

    C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,8 @@
 void print_alphabet_x10(void)
 {
 	char letter;
-	for(int i = 1 ; i <= 10 ; i++)
+
+	for (int i = 1 ; i <= 10 ; i++)
 		for(letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar("\n");
