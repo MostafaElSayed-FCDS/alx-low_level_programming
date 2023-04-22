80636fb0fed5bc64a0f0ccd11660ae01a076d741
Mostafa Elsayed <mostafaelsayed2172000@gmail.com>
2023-04-21 17:13:56 -0700

    a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+0-print_name.c
+
 #include "function_pointers.h"
 #include <stdio.h>
 /**
@@ -8,8 +10,8 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
-        if (name == NULL || f == NULL)
-                return;
+	if (name == NULL || f == NULL)
+		return;
 
-        f(name);
+	f(name);
 }
