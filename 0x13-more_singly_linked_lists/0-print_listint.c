1782a4329c7898035396a3edc408552fee22177e
MostafaElSayed-FCDS <mostafaelsayed2172000@gmail.com>
2023-04-30 20:01:29 -0700

    C

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- *print_listint - prints a linked lists
+ *print_listint - print elements of linked lists
  *@h: pointer to first node
  *
- * Return: size of list
+ * Return: number of nodes in linked list
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t = 0;
+	size_t j = 0;
 
 	while (h){
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
+		j++;
 	}
-	return (i);
+	return (j);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- *listint_len - prints a linked lists
+ *listint_len - prints num of element in linked lists
  *@h: pointer to first node
  *
- * Return: size of list
+ * Return: number of nodes in linked list
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t i = 0;
+	size_t j = 0;
 
         while (h)
 	{
 		h = h->next;
-                i++;
+                j++;
 	}
-        return (i);
+        return (j);
 }
